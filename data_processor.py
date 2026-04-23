fc488a45a8ec6cc195d7e3df99b3c990c9e8a588
pkhare18 <84315949+pkhare18@users.noreply.github.com>
2026-04-23 14:06:10 +0000

    data_processor

--- data_processor.py
+++ data_processor.py
@@ -1,11 +1,24 @@
 import pandas as pd
+import os
  
 DATA_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
+LOCAL_PATH = "sample_data/yellow_tripdata_2023-01.parquet"
+ 
+def download_data():
+    if not os.path.exists(LOCAL_PATH):
+        print("Downloading dataset...")
+        df = pd.read_parquet(DATA_URL)
+        df.to_parquet(LOCAL_PATH)
+        print("Saved locally!")
+    else:
+        print("Dataset already exists.")
  
 def load_data():
-    df = pd.read_parquet(DATA_URL)
+    download_data()
  
-    # Reduce size for performance
+    df = pd.read_parquet(LOCAL_PATH)
+ 
+    # Reduce size
     df = df.sample(n=50000, random_state=42)
  
     df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
@@ -13,5 +26,6 @@ def load_data():
  
     return df
 
-if __name__=="main":
-    print(load_data.head)
+if __name__=="__main__":
+    df=load_data()
+    print(df.head())
